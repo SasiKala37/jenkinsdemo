123db61d17f80eefaf4e77aad7e9d51f83b1b8b8
bridgelabz <SasiKala37>
2018-09-19 12:38:56 +0530

    changes jenkins commited...

--- src/main/java/com/bridgelabz/jenkins/JenkinsApplication.java
+++ src/main/java/com/bridgelabz/jenkins/JenkinsApplication.java
@@ -11,16 +11,10 @@ public class JenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
-		/*for(int i=1;i<=10;i++) {
-			System.out.println("Hello world------"+i);
-		}
-		String[] names= {"mamta","Shruthi","simran","Prateek","Swetha"};
-		for(int i=0;i<names.length;i++) {
-			System.out.println(names[i]);
-		}*/
+		
 	}
 	@GetMapping("/")
 	public String getMessage() {
-		return "hellooooo... world...";
+		return "hellooooo... world...jenkinsssssssss";
 	}
 }
