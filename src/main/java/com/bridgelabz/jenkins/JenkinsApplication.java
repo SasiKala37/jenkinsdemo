2b32eb5a365875f3316d89ac9bf5d9819026f6a0
bridgelabz <SasiKala37>
2018-09-19 11:08:51 +0530

    changes commited

--- src/main/java/com/bridgelabz/jenkins/JenkinsApplication.java
+++ src/main/java/com/bridgelabz/jenkins/JenkinsApplication.java
@@ -2,18 +2,25 @@ package com.bridgelabz.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
-		for(int i=1;i<=10;i++) {
+		/*for(int i=1;i<=10;i++) {
 			System.out.println("Hello world------"+i);
 		}
 		String[] names= {"mamta","Shruthi","simran","Prateek","Swetha"};
 		for(int i=0;i<names.length;i++) {
 			System.out.println(names[i]);
-		}
+		}*/
+	}
+	@GetMapping("/getMessage")
+	public String getMessage() {
+		return "hellooooo... world...";
 	}
 }
